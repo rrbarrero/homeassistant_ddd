ca3e158194fb822fe5465bea6a04ce7900d62581
Roberto Rodríguez <rrb@denexus.io>
2024-09-01 11:58:38 +0200

    refactor

--- src/tests/integration/dispatcher/test_remote_state_read.py
+++ src/tests/integration/dispatcher/test_remote_state_read.py
@@ -1,3 +1,4 @@
+from functools import partial
 from app.selection_algorithm import NaiveSelectionAlgorithm
 from domain.command_handlers.check_devices_command_handler import (
     CheckDevicesCommandHandler,
@@ -84,34 +85,37 @@ def test_dispatcher():
 
     dispatcher.run()
 
+    check_repo_state_at = partial(
+        aggregation_state_at, spy=spy, aggregate_repo=aggregate_repo
+    )
+
     # First read exceedance from HA
     assert spy[0].current_state.exceedance == 2000
-    assert aggregation_state_at(spy=spy, aggregate_repo=aggregate_repo, index=0).status == CurrentStateStatus.REMOTE_READ
+    assert check_repo_state_at(index=0).status == CurrentStateStatus.REMOTE_READ
 
     # 2nd power on first device 'aa'
     assert spy[1].device_changed.entity_id == "aa"
     assert spy[1].device_changed.state is True
     assert spy[1].device_changed.consumption == 700
-    assert aggregation_state_at(spy=spy, aggregate_repo=aggregate_repo, index=1).status == CurrentStateStatus.MANAGED_DEVICE
+    assert check_repo_state_at(index=1).status == CurrentStateStatus.MANAGED_DEVICE
 
     # 3rd exceedance is updated with the new consumption
     assert spy[2].current_state.exceedance == 1300
-    assert aggregation_state_at(spy=spy, aggregate_repo=aggregate_repo, index=2).status == CurrentStateStatus.STATE_RECALCULATED
+    assert check_repo_state_at(index=2).status == CurrentStateStatus.STATE_RECALCULATED
 
     # 4th power on second device 'cc'
     assert spy[3].device_changed.entity_id == "cc"
     assert spy[3].device_changed.state is True
     assert spy[3].device_changed.consumption == 600
-    assert aggregation_state_at(spy=spy, aggregate_repo=aggregate_repo, index=3).status == CurrentStateStatus.MANAGED_DEVICE
+    assert check_repo_state_at(index=3).status == CurrentStateStatus.MANAGED_DEVICE
 
     # 5th exceedance is updated with the new consumption
     assert spy[4].current_state.exceedance == 700
-    assert aggregation_state_at(spy=spy, aggregate_repo=aggregate_repo, index=4).status == CurrentStateStatus.STATE_RECALCULATED
+    assert check_repo_state_at(index=4).status == CurrentStateStatus.STATE_RECALCULATED
 
     # No more room for new devices because the marginal offset (hardcoded to 400)
     assert spy[5].device_changed is None
-    assert aggregation_state_at(spy=spy, aggregate_repo=aggregate_repo, index=5).status == CurrentStateStatus.MANAGED_DEVICE
-
+    assert check_repo_state_at(index=5).status == CurrentStateStatus.MANAGED_DEVICE
 
 def aggregation_state_at(spy: list, aggregate_repo: InMemoryAggregateRepository, index: int) -> CurrentState:
     aggregate_id = spy[index].current_state.aggregate_id.value
